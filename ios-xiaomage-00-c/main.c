2e9faa231937efeb6bfab85b27067fca8357ac60
phoenixfate <sm516116978@outlook.com>
2020-02-16 13:24:50 +0800

    Initial Commit

---
+++ ios-xiaomage-00-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ios-xiaomage-00-c
+//
+//  Created by Phoenix Fate on 2020/2/16.
+//  Copyright © 2020 Phoenix Fate. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
